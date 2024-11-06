05180c9cac89a55f6310da38d1d8c7498dfb7090
H66010 <hayashi23333@gmail.com>
2024-11-06 20:33:52 +0900

    Update Login_page_element.py

--- Pageobject/Login_page_element.py
+++ Pageobject/Login_page_element.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# ログインページの要素（セレクタ）
+# ログインページの要素（セレクタ,値）
 class LoginPageElement:
     username = (By.ID, "email")
     password = (By.ID, "password")
