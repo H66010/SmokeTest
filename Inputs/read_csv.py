4705038b23c2f67d30099e28792e1a6bb46066ac
H66010 <hayashi23333@gmail.com>
2024-11-06 20:33:29 +0900

    Update read_csv.py

--- Inputs/read_csv.py
+++ Inputs/read_csv.py
@@ -8,7 +8,7 @@ def read_csv_data(file_path, encoding='utf-8'):
         with open(file_path, newline='', encoding=encoding) as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                # 行の長さを確認し、適切なデータのみ追加
+              
                 if len(row) >= 2 and row[0] and row[1]:
                     usernames.append(row[0].strip())
                     passwords.append(row[1].strip())
